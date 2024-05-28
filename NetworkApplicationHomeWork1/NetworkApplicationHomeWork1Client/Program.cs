21d7e55f3822aed15b047f3507aef90a206d71ca
XOF0XC0000005 <leva95reva@yahoo.com>
2024-05-28 10:40:41 +0300

    initial commit

---
+++ NetworkApplicationHomeWork1/NetworkApplicationHomeWork1Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using NetworkApplicationHomeWork1Server;
+
+namespace NetworkApplicationHomeWork1Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SentMessage(args[0], args[1]);
+        }
+
+        public static void SentMessage(string from, string ip)
+        {
+            using (UdpClient udpClient = new UdpClient())
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+
+                while (true)
+                {
+                    string? messageText;
+
+                    do
+                    {
+                        Console.WriteLine("Введите сообщение.");
+                        messageText = Console.ReadLine();
+                    }
+                    while (string.IsNullOrEmpty(messageText));
+
+                    Message message = new Message() { Text = messageText, DateTime = DateTime.Now, NickFrom = from, NickTo = "Server" };
+                    string json = message.SerializeMessageToJson();
+                    byte[] data = Encoding.UTF8.GetBytes(json);
+                    udpClient.Send(data, data.Length, endPoint);
+
+                    byte[] buffer = udpClient.Receive(ref endPoint);
+                    if (buffer == null) break;
+
+                    var messageReceive = Encoding.UTF8.GetString(buffer);
+                    Console.WriteLine(messageReceive);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ NetworkApplicationHomeWork1/NetworkApplicationHomeWork1Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace NetworkApplicationHomeWork1Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server();
+        }
+
+        public static void Server()
+        {
+            using (UdpClient udpClient = new UdpClient(12345))
+            {
+                IPEndPoint ipEndpoint = new IPEndPoint(IPAddress.Any, 0);
+
+                Console.WriteLine("Сервер ждет сообщение от клиента");
+
+                while (true)
+                {
+                    byte[] buffer = udpClient.Receive(ref ipEndpoint);
+
+                    if (buffer == null) break;
+
+                    var messageText = Encoding.UTF8.GetString(buffer);
+                    Message? message = Message.DeserializeMessageFromJson(messageText);
+                    message?.Print();
+
+                    byte[] answer = Encoding.UTF8.GetBytes("Сообщение получено");
+                    udpClient.Send(answer, answer.Length, ipEndpoint);
+                }
+            }
+        }
+    }
+}
