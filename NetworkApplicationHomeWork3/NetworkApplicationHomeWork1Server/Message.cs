d0c03909a03fe15a753e5433feedb39b7683c4c4
XOF0XC0000005 <leva95reva@yahoo.com>
2024-06-05 13:15:07 +0300

    Initial commit

---
+++ NetworkApplicationHomeWork3/NetworkApplicationHomeWork1Server/Message.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace NetworkApplicationHomeWork1Server
+{
+    public class Message
+    {
+        public string? Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? NickFrom { get; set; }
+        public string? NickTo { get; set; }
+
+        public string SerializeMessageToJson() => JsonSerializer.Serialize(this);
+        public static Message? DeserializeMessageFromJson(string message) => JsonSerializer.Deserialize<Message>(message);
+        public void Print() => Console.WriteLine(ToString());
+        public override string ToString() => $"{DateTime} получено сообщение {Text} от {NickFrom}";
+    }
+}
